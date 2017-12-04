ee22392804c66796391977cc1040672870db449f
kbkk <sejntcik@gmail.com>
2017-12-04 16:19:41 +0100

    add day 4 - part two solution

---
+++ day_4/2.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input', {encoding: 'utf-8'});
+
+const out = input
+    .split('\r\n')
+    .map(row => row.split(' '))
+    .map(row => row.map(word => [...word].sort().join('')))
+    .map(row => +[...new Set(row)].length === row.length)
+    .reduce((a, b) => a + b);
+
+console.log(out);
