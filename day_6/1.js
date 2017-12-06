65ebae634a94574a08166901f0e2c07045f2e817
kbkk <sejntcik@gmail.com>
2017-12-06 13:45:07 +0100

    fix day 6 -part one solution

--- day_6/1.js
+++ day_6/1.js
@@ -6,29 +6,41 @@ const banks = fs
     .map(Number);
 
 const bankHistory = [[...banks]];
-let steps = 0;
 
-while (true) {
-  steps++;
+function calculateSteps() {
+  let steps = 0;
 
-  const busiestBank = banks.indexOf(Math.max(...banks));
-  let remainder = banks[busiestBank];
-  banks[busiestBank] = 0;
+  while (true) {
+    steps++;
 
-  for (let i = busiestBank + 1; remainder > 0; i++) {
-    const bank = (i >= banks.length) ? i % banks.length : i;
-    banks[bank] += 1;
-    remainder--;
-  }
+    const busiestBank = banks.indexOf(Math.max(...banks));
+    let remainder = banks[busiestBank];
+    banks[busiestBank] = 0;
 
-  if (!bankHistory.every(function (v) {
-        for (let i = 0; i < v.length; i++) {
-          if (v[0] !== banks[0])
-            return false;
+    for (let i = busiestBank + 1; remainder > 0; i++) {
+      const bank = (i >= banks.length) ? i % banks.length : i;
+      banks[bank] += 1;
+      remainder--;
+    }
+
+    for (let bankIndex = 0; bankIndex < bankHistory.length; bankIndex++) {
+      const bank = bankHistory[bankIndex];
+      let isEqual = true;
+
+      for (let i = 0; i < bank.length; i++) {
+        if (bank[i] !== banks[i]) {
+          isEqual = false;
+          break;
         }
-      })) break;
+      }
 
-  bankHistory.push([...banks]);
+      if (isEqual) return steps;
+    }
+    bankHistory.push([...banks]);
+  }
 }
 
-console.log(steps);
+console.log(calculateSteps());
+/*
+console.log(Math.max(...bankHistory
+    .map(row => Math.max(...row))));*/
